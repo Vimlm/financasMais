a59782d2343f060a17ba411c52a5c3f22cf156ca
Victor Martins <VictorMLMartins@icloud.com>
2023-10-01 17:56:27 -0300

    feat: :sparkles: Configuração Inicial
    Realizado a confiração inicial de ambiente de produção React, Vite Bun e Github

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+import LandingPage from './pages/LandingPage/LandingPage'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <LandingPage />
+)
---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import { Heart } from "@phosphor-icons/react";
+
+const LandingPage = () => {
+  return (
+    <>
+      <h1>{<Heart color="#AE2983" weight="fill" size={26} />} Hello World {<Heart color="#AE2983" weight="fill" size={26} />}</h1>
+      
+    </>
+  )
+}
+
+export default LandingPage
